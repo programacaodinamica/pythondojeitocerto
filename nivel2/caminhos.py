96b039d7f2bf1b1ec92f9bfc63f360831f1b45ec
Hallison Paz <hallpaz@impa.br>
2024-07-24 15:55:47 -0400

    after os

---
+++ nivel2/caminhos.py
@@ -0,0 +1,30 @@
+import os
+# absoluto ="/Users/hallpaz/Workspace/pgdinamica/pjc2/nivel2/caminhos.py"
+# relativo = "nivel2/caminhos.py"
+
+# DATA_DIR = "data"
+# print(os.path.abspath(DATA_DIR))
+
+# caminho = os.path.join(os.path.abspath(DATA_DIR), "lados.txt")
+# print(caminho)
+
+# print(os.listdir("nivel2"))
+
+# modulos_python = []
+# for nome in os.listdir("nivel2"):
+#     if nome.endswith(".py"):
+#         modulos_python.append(nome)
+# print(modulos_python)
+
+# print(os.path.join(
+#     os.path.abspath('.'), 'data', 'lados.txt'
+# ))
+
+OUTPUT_DIR = "saidas"
+nomearquivo = "mensagem.txt"
+
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+with open(os.path.join(OUTPUT_DIR, nomearquivo), 'w') as arq:
+    arq.write("Inscreva-se no canal Programação Dinâmica")
