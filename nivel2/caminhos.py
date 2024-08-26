ec1ad07053edb28b1fa29053f9a963fdbaa0891e
Hallison Paz <hallpaz@impa.br>
2024-08-26 20:08:44 -0300

    após preparação para curso

--- nivel2/caminhos.py
+++ nivel2/caminhos.py
@@ -1,28 +1,32 @@
 import os
-# absoluto ="/Users/hallpaz/Workspace/pgdinamica/pjc2/nivel2/caminhos.py"
-# relativo = "nivel2/caminhos.py"
 
-# DATA_DIR = "data"
-# print(os.path.abspath(DATA_DIR))
+# endereço absoluto vs relativo
+absoluto ="/Users/hallpaz/Workspace/pgdinamica/pjc2/nivel2/caminhos.py"
+relativo = "nivel2/caminhos.py"
+# obtendo endereço absoluto
+DATA_DIR = "data"
+print(os.path.abspath(DATA_DIR))
+# adicionando o nome de um arquivo a um endereço
+caminho = os.path.join(os.path.abspath(DATA_DIR), "lados.txt")
+print(caminho)
 
-# caminho = os.path.join(os.path.abspath(DATA_DIR), "lados.txt")
-# print(caminho)
+# obtendo o conteúdo de uma pasta
+print(os.listdir("nivel2"))
 
-# print(os.listdir("nivel2"))
+modulos_python = []
+for nome in os.listdir("nivel2"):
+    if nome.endswith(".py"):
+        modulos_python.append(nome)
+print(modulos_python)
 
-# modulos_python = []
-# for nome in os.listdir("nivel2"):
-#     if nome.endswith(".py"):
-#         modulos_python.append(nome)
-# print(modulos_python)
+# construindo um caminho mais complexo
+print(os.path.join(
+    os.path.abspath('.'), 'data', 'lados.txt'
+))
 
-# print(os.path.join(
-#     os.path.abspath('.'), 'data', 'lados.txt'
-# ))
-
+# criando um novo diretório
 OUTPUT_DIR = "saidas"
 nomearquivo = "mensagem.txt"
-
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
