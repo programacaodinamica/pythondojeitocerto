cdb1a656797ca1bde46981819d8d7d30aacff13d
Hallison Paz <hallpaz@impa.br>
2025-08-02 17:23:54 -0300

    Implementamos funções de aritméticas pelo lado direito para realizarmos operações entre frações e inteiros

--- nivel3/fracao.py
+++ nivel3/fracao.py
@@ -20,11 +20,19 @@ class Fracao:
             num_r = (num1 * den2) + (num2 * den1)
         return Fracao(num_r, den_r)
     
+    def __radd__(self, lhs):
+        if isinstance(lhs, int):
+            return self + lhs
+        raise TypeError(f"Operação + não suportada entre {type(self)} e {type(lhs)}")
+    
     def __neg__(self):
         return Fracao(-self.numerador, self.denominador)
     
     def __sub__(self, rhs):
         return self + (-rhs)
+    
+    def __rsub__(self, lhs):
+        return -self + lhs
     
     def __mul__(self, rhs):
         if isinstance(rhs, int):
@@ -35,12 +43,20 @@ class Fracao:
         den = self.denominador * den2
         return Fracao(num, den)
     
+    def __rmul__(self, lhs):
+        return self * lhs
+    
     def __truediv__(self, rhs):
         if isinstance(rhs, int):
             num, den = 1, rhs
         else:
             num, den = rhs.denominador, rhs.numerador
         return self * Fracao(num, den)
+    
+    def __rtruediv__(self, lhs):
+        if isinstance(lhs, int):
+            return Fracao(lhs, 1) / self
+        raise TypeError(f"Operação / não suportada entre {type(self)} e {type(lhs)}")
     
     def __lt__(self, rhs):
         num1, den1 = self.numerador, self.denominador
@@ -80,13 +96,25 @@ if __name__ == '__main__':
     # print(fracao.numerador, fracao.denominador)
     # print("A fração é:", fracao)
 
+    # Operações aritméticas com frações
     print(f"A soma das fracoes {fracao1} e {fracao2} é", fracao1 + fracao2)
     print(f"A diferença das fracoes {fracao1} e {fracao2} é", fracao1 - fracao2)    
     print(f"O produto das fracoes {fracao1} e {fracao2} é", fracao1 * fracao2)
     print(f"{fracao1} dividido por {fracao2} é", fracao1 / fracao2)
+    # Comparações entre frações
     print(f"A fração {fracao1} é menor que {fracao2}?", fracao1 < fracao2)
     print(f"A fração {fracao1} é maior que {fracao2}?", fracao1 > fracao2)
     print(f"A fração {fracao1} é igual a {fracao2}?", fracao1 == fracao2)
     print(f"A fração {fracao1} é menor ou igual a {fracao2}?", fracao1 <= fracao2)
     print(f"A fração {fracao1} é maior ou igual a {fracao2}?", fracao1 >= fracao2)
+
+    # Operações entre inteiros e frações
+    print(f"A soma de {fracao1} com 7 é", fracao1 + 7)
+    print(f"A soma de 7 com {fracao1}", 7 + fracao1)
+    print(f"A diferença entre {fracao1} e 7 é", fracao1 - 7)
+    print(f"A diferença entre 7 e {fracao1}", 7 - fracao1)
+    print(f"A multiplicação de {fracao1} por 7 é", fracao1 * 7)
+    print(f"A multiplicação de 7 por {fracao1}", 7 * fracao1)
+    print(f"A divisão de {fracao1} por 7 é", fracao1 / 7)
+    print(f"A divisão de 7 por {fracao1}", 7 / fracao1)
     
